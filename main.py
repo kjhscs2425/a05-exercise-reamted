171409bc306f01560a9fbdca2df85177a687fdbd
reamted <nroussos2028@kehillahstudent.org>
2024-10-31 11:18:19 -0700

    fever checker

--- main.py
+++ main.py
@@ -3,12 +3,17 @@
 `check_fever` should return `True` if the temperature is `100.4` or higher. For
 any lower temperature, it should return `False`.
 """
-def check_fever(temperature):
-  #YOUR CODE HERE
-  return
+def check_fever(temperature): 
+  if temperature > 100.4:
+    return True
+  else:
+    return False
+
 
 # Get temperature from user and convert to float
-temp = ...
+
+
+temp = int(input("Tell me a number: "))
 if check_fever(temp):
   print("You have a fever.")
 else:
